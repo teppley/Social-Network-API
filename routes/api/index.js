65b2862c015b22c67b77daaa5c9edc92ef673311
Tyler Eppley <teppley300@gmail.com>
2023-05-19 19:25:38 -0400

    corrected and wrote api index file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,31 +1,8 @@
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
 
-const {
-    getThought,
-    getSingleThought,
-    createThought,
-    updateThought,
-    deleteThought,
-    createReaction,
-    deleteReaction
-} = require('../../controllers/thoughtController');
-
-//GET all and POST thought
-router.route('/').get(getThought).post(createThought);
-
-//GET one thought, PUT and DELETE by iD
-router.route('/:thoughtId')
-.get(getSingleThought)
-.put(updateThought)
-.delete(deleteThought);
-
-//POST new reactions
-router.route('/:thoughtId/reactions')
-.post(createReaction);
-
-//DELETE reaction by ID
-router.route('/:thoughtId/reactions/:reactionId')
-.delete(deleteReaction);
-
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
