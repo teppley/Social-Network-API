d868ca35dc6a706571471d5a9f9ffb1ed803899c
Tyler Eppley <teppley300@gmail.com>
2023-05-20 21:27:59 -0400

    edited and fixed thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,22 +2,29 @@ const { User, Thought } = require("../models");
 
 module.exports = {
   // Get all thoughts
-  getThought(req, res) {
-    Thought.find({})
-      .then((thought) => res.json(thought))
-      .catch((err) => res.status(500).json(err));
+  async getThought(req, res) {
+    try {
+      const thought = await Thought.find({});
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
   // get single thought
-  getSingleThought(req, res) {
-    Thought.findOne({ _id: req.params.thoughtId })
-      .select("-__v")
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: "No Thought find with this ID!" })
-          : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId }).select("-__v");
+      
+      if (!thought) {
+        return res.status(404).json({ message: "No Thought found with this ID!" });
+      }
+  
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
+  
   //create a thought and push the created thought's _id to the associated user's thoughts array field
   createThought(req, res) {
     Thought.create(req.body)
